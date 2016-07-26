d763352aa4a4eb450d71756c490347176e2b07e7
liante0904 <liante0904@gmail.com>
2016-07-26 17:49:28 +0900

    160726 보충 commit

--- Private_Java/Parsing_Java/src/MBC/Html_Parsing.java
+++ Private_Java/Parsing_Java/src/MBC/Html_Parsing.java
@@ -13,16 +13,26 @@ public class Html_Parsing {
 	public void html() {
 		try {
 		      Document doc = Jsoup.connect("http://mini.imbc.com/manager/SelectView.asp?PROG_CD=FM4U000001271&SeqNo=1061&PG=1").get(); //웹에서 내용을 가져온다.
-		   //   System.out.println(doc.title());
+		      Elements head = doc.select("div.s_tb_temp h3");
+		      System.out.println(head.text());
 		      Elements title = doc.select("td p");
 		      String txt = title.text();
-		      System.out.println(txt);
+		    //  System.out.println(txt); // 제목
 		      
 		      
 		      Elements contents = doc.select("td.td_artist"); //내용을 가져온다.
 		    //   String text = contents.attr("title"); //attr은 기본적으로 String형이다. 
 		      String text = contents.text();
-		       System.out.println(text);
+		      // System.out.println(text); // 가수
+		       
+		       if (doc != null) {
+				 Elements title1 = doc.select("td p");
+			      Elements artist = doc.select("td.td_artist");
+				 for (int i = 0; i < title1.size(); i++) {
+					System.out.println( artist.get(i).text() + "  -  " + title1.get(i).text() );
+				}
+				      
+			}
 		} catch (IOException e) { //Jsoup의 connect 부분에서 IOException 오류가 날 수 있으므로 사용한다.
 		      e.printStackTrace();
 		}
--- Private_Java/Parsing_Java/src/SBS/Html_Parsing.java
+++ Private_Java/Parsing_Java/src/SBS/Html_Parsing.java
@@ -8,20 +8,35 @@ import org.jsoup.select.Elements; //import Jsoup
 
 public class Html_Parsing {
 
+	String url = "http://radio.sbs.co.kr/music/wizard_broadcast_renew.jsp?vod_id=V0000330602&year=2016&month=07&day=25&position_code=romanticage";
+			
 	
 	public void html() {
 		try {
-		      Document doc = Jsoup.connect("http://radio.sbs.co.kr/music/wizard_broadcast_renew.jsp?vod_id=V0000330602&year=2016&month=07&day=25&position_code=romanticage").get(); //웹에서 내용을 가져온다.
-		   //   System.out.println(doc.title());
-		      Elements title = doc.select("span.rml_songname");
-		      String txt = title.text();
-		    //  System.out.println(txt);
+		      Document doc = Jsoup.connect(url).get(); //웹에서 내용을 가져온다.
+		
+		      
+		      Elements head = doc.select("div.schedule_date");
+		      String txt = head.text();
+		      if (url.contains("code=romanticage")) {
+		    	  txt += "최백호의 낭만시대";
+			}
+		      System.out.println(txt.replace("닫기", ""));
 		      
 		      
 		      Elements contents = doc.select("span.rml_singer_name"); //내용을 가져온다.
 		    //   String text = contents.attr("title"); //attr은 기본적으로 String형이다. 
 		      String text = contents.text();
-		      System.out.println(txt + "\n - " + text);
+		      // System.out.println(txt + "\n - " + text);
+		      
+		      
+		       if (doc != null) {
+					 Elements title1 = doc.select("span.rml_songname");
+				      Elements artist = doc.select("span.rml_singer_name");
+					 for (int i = 0; i < title1.size(); i++) {
+						System.out.println( artist.get(i).text() + "  -  " + title1.get(i).text() );
+					}
+		       }
 		} catch (IOException e) { //Jsoup의 connect 부분에서 IOException 오류가 날 수 있으므로 사용한다.
 		      e.printStackTrace();
 		}
